d3225277b48edb07fa221e912dc022e733eb5833
sghoshcvc <sghosh@cvc.uab.es>
2015-04-12 02:09:47 +0200

    env prep

---
+++ main_compute_integral.m
@@ -0,0 +1,10 @@
+%function [ output_args ] = main_compute_integral
+%UNTITLED2 Summary of this function goes here
+%   Detailed explanation goes here
+
+opts =Prep_env();
+prepare_images(opts)
+
+
+%end
+
